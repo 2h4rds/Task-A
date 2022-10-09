99e8a657ee2a4fffb2f344378fcdf990f5fe46bf
Kirill Sinkov <99603004+2h4rds@users.noreply.github.com>
2022-10-09 16:46:54 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,7 +2,12 @@
 // task 1
 #include <iostream>
 using namespace std;
-int main() {
-	int a;
-	cout << &a << endl;
+int main(){
+	int A;
+	cin >> A;
+	int fd;
+	int sd;//fd,sd
+	sd = A % 10;
+	sd = A * 10;
+	cout << sd;
 }
